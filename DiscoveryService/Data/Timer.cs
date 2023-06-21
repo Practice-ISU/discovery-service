34fd1fb91ffec089a9fc2ce253345d01bf105fd9
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-21 21:46:42 +0800

    Create timer for the ping of microservices

---
+++ DiscoveryService/Data/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace DiscoveryService.Data
+{
+    public class TimerRequests
+    {
+        private readonly Timer _timer;
+        private readonly int _timeSleep = 5 * 60 * 1000;
+
+        public TimerRequests()
+        {
+            _timer = new Timer(_ => Task.Run(SendRequestsToServer),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromMilliseconds(_timeSleep));
+        }
+
+        private async Task SendRequestsToServer()
+        {
+            foreach (var service in InMemoryStorageServices.GetAll())
+            {
+                var channel = GrpcChannel.ForAddress(service.Value);
+                var client = new DiscoveryPing.DiscoveryPing.DiscoveryPingClient(channel);
+
+                try
+                {
+                    var response = await client.PingAsync(new DiscoveryPing.PingRequest { Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), ServiceName = service.Key });
+                    if (response.Success)
+                    {
+                        continue;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Microservice {service.Key} is not responding\nremoving from Available list\n");
+                    InMemoryStorageServices.Delete(service.Key);
+                }
+            }
+        }
+    }
+}
--- DiscoveryService/Program.cs
+++ DiscoveryService/Program.cs
@@ -1,4 +1,4 @@
-using DiscoveryService.Services;
+using DiscoveryService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +11,11 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscoveryService.Services.DiscoveryService>();
+app.MapGrpcService<DiscoveryService.Services.ServiceRegistration>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+var timerRequests = new TimerRequests();
 
 app.Run();
