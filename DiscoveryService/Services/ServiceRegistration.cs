800f392dbed1f6511a4a01298bcc83bf73435de3
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-21 21:11:21 +0800

    Fix for ServiceRegistration

---
+++ DiscoveryService/Services/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using DiscoveryRegistration;
+using DiscoveryService.Data;
+using Grpc.Core;
+
+namespace DiscoveryService.Services
+{
+    public class ServiceRegistration : DiscoveryRegistration.ServiceRegistration.ServiceRegistrationBase
+    {
+        public override Task<ServiceResponse> Registration(ServiceRequest request, ServerCallContext context)
+        {
+            InMemoryStorageServices.Set(request.ServiceName, request.Channel);
+
+            return Task.FromResult(new ServiceResponse
+            {
+                Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"),
+                Success = true
+            });
+        }
+    }
+}
