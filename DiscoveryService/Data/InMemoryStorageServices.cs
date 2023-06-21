383cc7b290dad3621f279109fec98bf9999e7720
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-21 21:02:09 +0800

    Create InMemoryStorage for Services

---
+++ DiscoveryService/Data/InMemoryStorageServices.cs
@@ -0,0 +1,34 @@
+﻿namespace DiscoveryService.Data
+{
+    public static class InMemoryStorageServices
+    {
+        private static Dictionary<string, string> _data = new Dictionary<string, string>();
+
+        public static void Set(string serviceName, string channel)
+        {
+            if (_data.ContainsKey(serviceName))
+            {
+                _data[serviceName] = channel;
+            }
+            else
+            {
+                _data.Add(serviceName, channel);
+            }
+        }
+
+        public static string? GetChannel(string serviceName)
+        {
+            if (_data.ContainsKey(serviceName))
+            {
+                return _data[serviceName];
+            }
+
+            return null;
+        }
+
+        public static Dictionary<string, string> GetAll()
+        {
+            return _data;
+        }
+    }
+}
