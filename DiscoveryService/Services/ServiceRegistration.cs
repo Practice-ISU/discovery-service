f7800f8fca8dab0b8f399aa41d10fa9febfa387e
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-06-21 22:21:48 +0800

    Add check for Microservice Registration

--- DiscoveryService/Services/ServiceRegistration.cs
+++ DiscoveryService/Services/ServiceRegistration.cs
@@ -8,6 +8,15 @@ namespace DiscoveryService.Services
     {
         public override Task<ServiceResponse> Registration(ServiceRequest request, ServerCallContext context)
         {
+            if (request.ServiceName=="" || request.Channel == "")
+            {
+                return Task.FromResult(new ServiceResponse
+                {
+                    Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"),
+                    Success = false
+                });
+            }
+
             InMemoryStorageServices.Set(request.ServiceName, request.Channel);
 
             return Task.FromResult(new ServiceResponse
